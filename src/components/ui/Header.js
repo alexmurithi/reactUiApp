0c15ab2f6652247eaf553056fc3b8fcd4df0abbc
Alex Murithi <gitsaleks@gmail.com>
2021-04-11 17:03:39 +0300

    inline styles

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/styles';
 
 
 function ElevationScroll(props) {
@@ -21,15 +22,28 @@ function ElevationScroll(props) {
     });
   }
 
+  const useStyles =makeStyles(theme=>({
+      toolbarMargin:{
+          ...theme.mixins.toolbar
+      }
+  }))
+
 export default function Header(props){
+
+    const classes =useStyles()
+
   return(
-    <ElevationScroll>
-       <AppBar position="fixed">
-         <Toolbar > 
-            <Typography variant="h3">Arc Development</Typography>
-         </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+        <ElevationScroll>
+            <AppBar position="fixed">
+                <Toolbar > 
+                    <Typography variant="h3">Arc Development</Typography>
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
+        <div className={classes.toolbarMargin}></div>
+    </React.Fragment>
+   
       
   )
 }
